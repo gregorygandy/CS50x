b6ef08a983d8648ed0dc4b42eb15e1102462b30d
Gregory Gandy <gandyg@oregonstate.edu>
2021-12-15 01:42:59 +0000

    completed tournament.py and restarting recover.c

--- lab6/tournament.py
+++ lab6/tournament.py
@@ -14,12 +14,23 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
-    teams =  #store each team as a dictionary
+    teams = []
     # TODO: Read teams into memory from file
+    with open(sys.argv[1], "r") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            teamdict = {"team": row[0], "rating": int(row[1])}
+            teams.append(teamdict)
 
-    counts = {} #the keys are the names of the teams, values are the number of tournaments a given team has won.
+    counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
@@ -49,7 +60,9 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]['team']
 
 
 if __name__ == "__main__":
--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,10 +1,12 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
-// Open a new JPEG fil
+// Open a new JPEG file
 // Write 512 bytes until a new JPEG is found
 // Stop at end of file
+
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -14,47 +16,21 @@ int main(int argc, char *argv[])
         return 1;
     }
     
-    // open memory card file
+    // open input, memory card, file
     FILE *memory_card = fopen(argv[1], "r");
     if (memory_card == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
-    uint8_t BYTE;
-    char filename[512];
     
-    while (fread(&BYTE, 512, 1, memory_card)) // repeat until end of card
-    {
-        
-        if (BYTE[0] == 0xff & BYTE[1] == 0xd8 & BYTE[2] == 0xff & ((BYTE[3] & 0xf0) == 0xe0)) // if start of new JPEG
+    BYTE buffer;
+    while (fread(&buffer, 512, 1, memory_card))
         {
-            if (jpeg_number == 0) // if first JPEG
-            {
-                sprintf(filename, "%03i.jpg", jpeg_number); // make sure file name has enough memory (charectors) to represent names
-                FILE *img = fopen(filename, "w"); // write data from memory card
-                fwrite(&BYTE, 512, 1, img);
-                jpeg_number = 1;
-            }
-            else // if already found JPEG, open up new file to continue writing
-            {
-                fclose(img);
-                sprintf(filename, "%03i.jpg", jpeg_number); // make sure file name has enough memory (charectors) to represent names
-                FILE *img = fopen(filename, "w"); // write data from memory card
-                fwrite(&BYTE, 512, 1, img);
-                jpeg_number += 1;
-            }
+            printf("1");
         }
-        else // not start of new JPEG
-        {
-            if (jpeg_number != 0) // condition if fread has got to the end of the file or not.
-            {
-                fwrite(&BYTE, 512, 1, img);
-            }
-            else // if there are no more JPEGs left to find
-            { 
-                fclose(memory_card);
-            }
-        }
-    }
+    
+    // Close memory card
+    fclose(memory_card);
+    return 0;
 }
